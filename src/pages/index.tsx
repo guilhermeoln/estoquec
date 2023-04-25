8a2402258543108e0c2ef8589b56a54b8825f251
jose guilherme <guilhermedev2002@gmail.com>
2023-04-25 15:06:50 -0300

    feat(Home): Criação da página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,40 @@
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+import Image from "next/image";
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <Flex
+      width="100%"
+      height="100vh"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Text fontSize="40px" marginBottom="50px">
+        ESTOQUEC
+      </Text>
+      <form>
+        <FormControl>
+          <FormLabel>Usuário</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <FormControl marginTop="20px">
+          <FormLabel>Senha</FormLabel>
+          <Input type="password" />
+        </FormControl>
+        <Button width="100%" marginTop="30px" bgColor="black" color="white">
+          Entrar
+        </Button>
+      </form>
+    </Flex>
+  );
 }
