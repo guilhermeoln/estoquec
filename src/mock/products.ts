d9108683fb7b9c179304bf625f36a22f2cc5aaf6
jose guilherme <guilhermedev2002@gmail.com>
2023-05-05 16:12:31 -0300

    feat(Catalog): Criação do schemaValidation do formulário com HookForm/yup

--- src/mock/products.ts
+++ src/mock/products.ts
@@ -4,19 +4,25 @@ const productsInitial: IProduct[] = [
   {
     imei: "112233445566",
     description: "Iphone 12",
-    category: "Pro Max",
+    category: "IPHONE",
+    model: "PRO",
+    memory: "256GB",
     price: 2000,
   },
   {
     imei: "112244445566",
     description: "Iphone 14",
-    category: "Pro",
+    model: "PRO",
+    category: "IPHONE",
+    memory: "128GB",
     price: 5200.5,
   },
   {
     imei: "112266445566",
     description: "Iphone XS",
-    category: "MAX",
+    category: "IPHONE",
+    model: "PRO",
+    memory: "64GB",
     price: 1800.3,
   },
 ];
--- src/types/IProduct.ts
+++ src/types/IProduct.ts
@@ -2,6 +2,8 @@ export interface IProduct {
   imei: string;
   description: string;
   category: string;
+  model: string;
+  memory?: string;
   price: number;
   quantifyBuy?: number;
 }
